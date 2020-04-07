fb1752b60c60b2aa5e4a3e878fd1708ff9893eda
DI CARLO Diego <diego.di-carlo@inria.fr>
2020-04-07 18:35:51 +0200

    update gui

--- src/visualization/interactive_rir2toa.py
+++ src/visualization/interactive_rir2toa.py
@@ -29,9 +29,9 @@ datasets = ['000000', '010000', '011000', '011100', '011110', '011111',
 ## INITIALIZE FIGURE
 scaling = 0.8
 fig = plt.figure(figsize=(16*scaling, 9*scaling))
-ax1 = fig.add_subplot(221, xlim=(-0.1, 2), ylim=(-0.1, 1))
-ax2 = fig.add_subplot(222, xlim=(-0.1, 2), ylim=(-0.1, 1), sharex=ax1, sharey=ax1)
-ax3 = fig.add_subplot(212, xlim=(-0.1, 2), ylim=(-0.1, 1), sharex=ax1, sharey=ax1)
+ax1 = fig.add_subplot(221, xlim=(20, 2000), ylim=(-0.05, 1.05))
+ax2 = fig.add_subplot(222, xlim=(20, 2000), ylim=(-0.05, 1.05), sharex=ax1, sharey=ax1)
+ax3 = fig.add_subplot(212, xlim=(20, 2000), ylim=(-0.05, 1.05), sharex=ax1, sharey=ax1)
 
 plt.subplots_adjust(top=0.90)
 plt.subplots_adjust(left=0.15)
@@ -46,22 +46,21 @@ Fs = params['Fs']
 
 taus_list = [r'%d $\tau_{%s}^{%d}$' % (k, toa_note['wall'][k, 0, 0, 0].decode(), toa_note['order'][k, 0, 0, 0]) for k in range(K)]
 
+# # Print the dataset name
+# wall_code_name = 'fcwsen'
+# wall_code = [int(i) for i in list(datasets[d])]
+# curr_walls = [wall_code_name[w]
+#                 for w, code in enumerate(wall_code) if code == 1]
+# ax.annotate(datasets[d], [50, 0.07])
+# ax.annotate('|'.join(curr_walls), [50, 0.025])
 
-def plot_rirs_and_note(rirs, note, i, j, selected_k, ax, visibility):
+def plot_rirs_and_note(rirs, note, i, j, selected_k, ax, visibility, dp_extreme):
     for d in range(D):
         if visibility[d]:
             rir_to_plot = rirs[:, d]
             rir_to_plot = rir_to_plot / np.max(np.abs(rir_to_plot), axis=0)
 
             ax.plot(rir_to_plot**2, alpha=.8, label=datasets[d])
-
-        # # Print the dataset name
-        # wall_code_name = 'fcwsen'
-        # wall_code = [int(i) for i in list(datasets[d])]
-        # curr_walls = [wall_code_name[w]
-        #                 for w, code in enumerate(wall_code) if code == 1]
-        # ax.annotate(datasets[d], [50, 0.07])
-        # ax.annotate('|'.join(curr_walls), [50, 0.025])
 
     # plot the echo information
     for k in range(K):
@@ -76,7 +75,6 @@ def plot_rirs_and_note(rirs, note, i, j, selected_k, ax, visibility):
 
         ax.annotate( r'$\tau_{%s}^{%d}$' % (wall.decode(), order), [toa*Fs, 0.25], fontsize=12)
 
-    ax.set_ylim([-0.05, 2.2])
     ax.set_title('RIRs dataset %s\nmic %d, src %d' % (datasets[0], i, j))
     pass
 
@@ -100,13 +98,13 @@ def plot_rirs_and_note_merged(rirs, note, i, j, selected_k, ax, fun= lambda x : 
         ax.annotate(r'$\tau_{%s}^{%d}$' % (wall.decode(), order), [
                     toa*Fs, 0.25], fontsize=12)
 
-    ax.set_ylim([-0.05, 1.1])
     pass
 
 
 ## EDIT PLOT VISIBILITY
 rax = plt.axes([0.01, 0.6, 0.1, 0.30])
-check = CheckButtons(rax, datasets, [True for d in datasets])
+initial_validity = [True if d == 1 else False for d, dataset in enumerate(datasets)]
+check = CheckButtons(rax, datasets, initial_validity)
 
 ## EDIT TAU LOCATIONS
 txt_boxes_tau = []
@@ -122,67 +120,22 @@ dp_box1 = TextBox(rax, 'Before', initial='10')
 rax = plt.axes([0.93, 0.915, 0.05, 0.03])
 dp_box2 = TextBox(rax, 'After', initial='20')
 rax = plt.axes([0.93, 0.88, 0.05, 0.03])
-dp_box2 = Button(rax, 'Do it')
+dp_box = Button(rax, 'Do it')
 
 # Buttons
 # ax_save_tau = plt.axes([0.01, 0.90, 0.04, 0.02])  # x, y, w, h
 # bt_save_tau = Button(ax_save_tau, 'save')
 
-# ax_which_tau = plt.axes([0.01, 0.15, 0.12, 0.40])  # x, y, w, h
-# bt_select_tau = RadioButtons(ax_which_tau, taus_list)
-
-# ax_load_tau = plt.axes([0.05, 0.90, 0.04, 0.02])  # x, y, w, h
-# bt_load_tau = Button(ax_load_tau, 'load')
-# ax_reset_curr_tau = plt.axes([0.01, 0.87, 0.04, 0.02])  # x, y, w, h
-# bt_reset_curr_tau = Button(ax_reset_curr_tau, r'reset $\tau$')
-
-# ax_xlim0_pprev = plt.axes([0.87, 0.04, 0.02, 0.02])
-# bt_xlim0_pprev = Button(ax_xlim0_pprev, '<<')
-# ax_xlim0_prev = plt.axes([0.89, 0.04, 0.02, 0.02])
-# bt_xlim0_prev = Button(ax_xlim0_prev, '<')
-# ax_xlim0_next = plt.axes([0.915, 0.04, 0.02, 0.02])
-# bt_xlim0_next = Button(ax_xlim0_next, '>')
-# ax_xlim0_nnext = plt.axes([0.935, 0.04, 0.02, 0.02])
-# bt_xlim0_nnext = Button(ax_xlim0_nnext, '>>')
-
-# ax_xlim1_pprev = plt.axes([0.87, 0.07, 0.02, 0.02])
-# ax_xlim1_prev = plt.axes([0.89, 0.07, 0.02, 0.02])
-# ax_xlim1_next = plt.axes([0.915, 0.07, 0.02, 0.02])
-# ax_xlim1_nnext = plt.axes([0.935, 0.07, 0.02, 0.02])
-# bt_xlim1_pprev = Button(ax_xlim1_pprev, '<<')
-# bt_xlim1_next = Button(ax_xlim1_next, '>')
-# bt_xlim1_prev = Button(ax_xlim1_prev, '<')
-# bt_xlim1_nnext = Button(ax_xlim1_nnext, '>>')
-
-# ax_move_tau_pprev = plt.axes([0.87, 0.01, 0.02, 0.02])
-# ax_move_tau_prev = plt.axes([0.89, 0.01, 0.02, 0.02])
-# ax_move_tau_next = plt.axes([0.915, 0.01, 0.02, 0.02])
-# ax_move_tau_nnext = plt.axes([0.935, 0.01, 0.02, 0.02])
-# bt_move_tau_pprev = Button(ax_move_tau_pprev, '<<')
-# bt_move_tau_prev = Button(ax_move_tau_prev, '<')
-# bt_move_tau_next = Button(ax_move_tau_next, '>')
-# bt_move_tau_nnext = Button(ax_move_tau_nnext, '>>')
-
-# # Slider
-# ax_move_tau = plt.axes([0.1, 0.01, 0.70, 0.02])  # x, y, w, h
-# sl_move_tau = Slider(ax_move_tau, r'current $\tau$', 0, 8000, valinit=200, valstep=0.01)
-# ax_set_xlim0 = plt.axes([0.1, 0.04, 0.70, 0.02])  # x, y, w, h
-# ax_set_xlim1 = plt.axes([0.1, 0.07, 0.70, 0.02])  # x, y, w, h
-# sl_set_xlim0 = Slider(ax_set_xlim0, 'x min', 0, 7990, valinit=0, valstep=1)
-# sl_set_xlim1 = Slider(ax_set_xlim1, 'x max', 10, 8000, valinit=3000, valstep=1)
-
-# # Text
-# ax_text_tau = plt.axes([0.05, 0.84, 0.04, 0.02])  # x, y, w, h
-# tx_text_tau = TextBox(ax_text_tau, 'Set tau')
-# ax_text_mic = plt.axes([0.05, 0.81, 0.04, 0.02])  # x, y, w, h
-# tx_text_mic = TextBox(ax_text_mic, 'Set mic')
-# ax_text_src = plt.axes([0.05, 0.78, 0.04, 0.02])  # x, y, w, h
-# tx_text_src = TextBox(ax_text_src, 'Set src')
+# Text
+ax_text_mic = plt.axes([0.06, 0.95, 0.05, 0.03])  # x, y, w, h
+tx_text_mic = TextBox(ax_text_mic, 'Set mic ', initial='0')
+ax_text_src = plt.axes([0.06, 0.915, 0.05, 0.03])  # x, y, w, h
+tx_text_src = TextBox(ax_text_src, 'Set src ', initial='0')
 
 
 class Callbacks():
 
-    def __init__(self, toa_note, rirs, all_rirs_clean, datasets, fig, axes=[ax1, ax2]):
+    def __init__(self, toa_note, rirs, all_rirs_clean, datasets, initial_validity, fig, axes=[ax1, ax2]):
         self.xlim = [0, 3000]
         self.all_rirs = rirs
         self.all_rirs_clean = all_rirs_clean
@@ -195,8 +148,11 @@ class Callbacks():
         self.Fs = 48000
         self.k = 0
 
+        self.dp_extreme = [int(self.toa_note['toa'][0, self.i, self.j, 0]*self.Fs)-10,
+                           int(self.toa_note['toa'][0, self.i, self.j, 0]*self.Fs)-30]
+
         self.datasets = datasets
-        self.visibility = [True for d in datasets]
+        self.visibility = initial_validity
 
         self.axes = axes
         self.fig = fig
@@ -205,11 +161,13 @@ class Callbacks():
         self.val = self.toa_note['toa'][self.idx, self.i, self.j, 0]
         self._update()
 
+
     def save_tau_to_file(self, foo):
         path = './data/interim/manual_annotation/'
         filename = 'src-%d-mic-%d.pkl' % (self.j, self.i)
         save_to_pickle(path + filename, self.toa_note)
         print('Annotation saved to file:', filename)
+
 
     def load_tau_note(self, bar):
         path = './data/interim/manual_annotation/'
@@ -218,13 +176,16 @@ class Callbacks():
         print('Annotation loaded from:', filename)
         self._update()
 
+
     def set_mic(self, i):
         self.i = i
         self._update()
 
+
     def set_src(self, j):
         self.j = j
         self._update()
+
 
     def set_idx(self, idx):
         if idx is not None:
@@ -233,6 +194,7 @@ class Callbacks():
         tau = self.toa_note['toa'][self.idx, self.i, self.j, 0]*self.Fs
         self.xlim = [tau-100, tau+100]
         self._update()
+
 
     def set_val(self, val=None, proc=None):
         if val is not None:
@@ -243,6 +205,15 @@ class Callbacks():
 
         self._update()
 
+    def set_extreme_dp_deconvolution(self, x, i):
+        self.dp_extreme[i] = x
+        print('Set %d extreme to %d' % (i, x))
+        self.axes[0].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
+        self.axes[1].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
+        self.axes[2].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
+
+
+
     def set_xlim(self, i, val=None, proc=None):
         if val is not None:
             self.xlim[i] = val
@@ -250,23 +221,62 @@ class Callbacks():
             self.xlim[i] += proc
         self._update()
 
+
     def set_visibility(self, label):
         index = self.datasets.index(label)
         self.visibility[index] = not self.visibility[index]
         self._update()
+
 
     def reset_tau(self, bar):
         self.toa_note['toa'][self.idx, self.i, self.j, 0] =  \
             self.toa_note_backup['toa'][self.idx, self.i, self.j, 0]
         self._update()
 
-    def _update(self):
+    def set_toa(self):
         # update taus
         self.toa_note['toa'][self.idx, self.i, self.j, :] = self.val
+
+    def set_tau_val(self, val, i):
+        # update taus
+        self.idx = i
+        self.toa_note['toa'][self.idx, self.i, self.j, :] = val/self.Fs
+        print('Updated tau %d to %1.2f' %
+              (self.idx, self.toa_note['toa'][self.idx, self.i, self.j, 0]))
+        self._update()
+
+
+    def dp_deconv(self, event):
+        L, I, J, D = self.all_rirs.shape
+
+        # take the dp from... anechoic
+        a = self.dp_extreme[0]
+        b = self.dp_extreme[1]
+        dp = self.all_rirs[a:b, self.i, self.j, 0]
+        p = np.argmax(np.abs(dp))
+        print(p)
+
+        print('Deconvolving with DP with dp in [%d, %d]' % (a, b))
+        for d in range(D):
+            rir = self.all_rirs[:, self.i, self.j, d]
+            dp_deconv = np.real(np.fft.ifft(np.fft.fft(rir, L) / np.fft.fft(dp, L)))[:L-p]
+            # restore the direct path
+            dp_deconv = np.concatenate([np.zeros(p), dp_deconv])
+            self.all_rirs_clean[:, self.i, self.j, d] = dp_deconv
+        print('Done')
+        self._update()
+
+
+
+    def _update(self):
+        xlim = self.axes[0].get_xlim()
+        ylim = self.axes[0].get_ylim()
+
+        print('Updating')
+        print(self.i, self.j)
         # update current rirs
         self.curr_rirs = self.all_rirs[:, self.i, self.j, :]
         self.curr_rirs_clean = self.all_rirs_clean[:, self.i, self.j, :]
-        print(self.i, self.j)
 
         ## UPDATE PLOTS
         # clear axes
@@ -274,10 +284,12 @@ class Callbacks():
         self.axes[1].clear()
         self.axes[2].clear()
 
-        self.axes[0].set_xlim(self.xlim)
+        self.axes[0].set_xlim(xlim)
+        self.axes[0].set_ylim(ylim)
 
-        plot_rirs_and_note(self.curr_rirs, self.toa_note, self.i, self.j, self.idx, self.axes[0], self.visibility)
-        plot_rirs_and_note(self.curr_rirs_clean, self.toa_note, self.i, self.j, self.idx, self.axes[1], self.visibility)
+        print(self.idx)
+        plot_rirs_and_note(self.curr_rirs, self.toa_note, self.i, self.j, self.idx, self.axes[0], self.visibility, self.dp_extreme)
+        plot_rirs_and_note(self.curr_rirs_clean, self.toa_note, self.i, self.j, self.idx, self.axes[1], self.visibility, self.dp_extreme)
 
         # # peak picking of 2nd axis
         # mean_rirs_to_plot = np.max(self.curr_rirs_clean**2, axis=-1)
@@ -292,37 +304,26 @@ class Callbacks():
 
 
 # init
-cb = Callbacks(cp(toa_note), all_rirs, all_rirs, datasets, fig, axes=[ax1, ax2, ax3])
+cb = Callbacks(cp(toa_note), all_rirs, cp(all_rirs), datasets, initial_validity, fig, axes=[ax1, ax2, ax3])
 
 check.on_clicked(cb.set_visibility)
 
-# sl_move_tau.on_changed(lambda val: cb.set_val(val))
-# sl_set_xlim0.on_changed(lambda val: cb.set_xlim(0, val))
-# sl_set_xlim1.on_changed(lambda val: cb.set_xlim(1, val))
-
-# bt_select_tau.on_clicked(lambda idx: cb.set_idx(idx=idx))
-# bt_move_tau_pprev.on_clicked(lambda idx: cb.set_val(proc=-1))
-# bt_move_tau_prev.on_clicked(lambda idx: cb.set_val(proc=-.1))
-# bt_move_tau_next.on_clicked(lambda idx: cb.set_val(proc=+.1))
-# bt_move_tau_nnext.on_clicked(lambda idx: cb.set_val(proc=+1))
-
-# bt_xlim0_pprev.on_clicked(lambda x: cb.set_xlim(0, proc=-5))
-# bt_xlim0_prev.on_clicked(lambda x: cb.set_xlim(0, proc=-1))
-# bt_xlim0_next.on_clicked(lambda x: cb.set_xlim(0, proc=+1))
-# bt_xlim0_nnext.on_clicked(lambda x: cb.set_xlim(0, proc=+5))
-
-# bt_xlim1_pprev.on_clicked(lambda x: cb.set_xlim(1, proc=-5))
-# bt_xlim1_prev.on_clicked(lambda x: cb.set_xlim(1, proc=-1))
-# bt_xlim1_next.on_clicked(lambda x: cb.set_xlim(1, proc=+1))
-# bt_xlim1_nnext.on_clicked(lambda x: cb.set_xlim(1, proc=+5))
+dp_box1.on_submit(lambda string: cb.set_extreme_dp_deconvolution(int(string), 0))
+dp_box2.on_submit(lambda string: cb.set_extreme_dp_deconvolution(int(string), 1))
+dp_box.on_clicked(cb.dp_deconv)
 
 # bt_save_tau.on_clicked(cb.save_tau_to_file)
 # bt_load_tau.on_clicked(cb.load_tau_note)
-# bt_reset_curr_tau.on_clicked(cb.reset_tau)
 
-# tx_text_mic.on_submit(lambda i: cb.set_mic(int(i)))
-# tx_text_src.on_submit(lambda j: cb.set_src(int(j)))
+tx_text_mic.on_submit(lambda i: cb.set_mic(int(i)))
+tx_text_src.on_submit(lambda j: cb.set_src(int(j)))
 
-cb.set_xlim(0, 20)
-cb.set_xlim(1,1000)
+txt_boxes_tau[0].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 0))
+txt_boxes_tau[1].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 1))
+txt_boxes_tau[2].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 2))
+txt_boxes_tau[3].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 3))
+txt_boxes_tau[4].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 4))
+txt_boxes_tau[5].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 5))
+txt_boxes_tau[6].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 6))
+
 plt.show()
