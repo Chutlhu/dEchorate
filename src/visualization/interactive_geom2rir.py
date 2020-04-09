d8c49f8cadb5ab41e67ea1cfc1629f3761392045
DI CARLO Diego <diego.di-carlo@inria.fr>
2020-04-09 17:14:12 +0200

    updated gui

--- src/visualization/interactive_geom2rir.py
+++ src/visualization/interactive_geom2rir.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from matplotlib.widgets import Slider, RadioButtons
+from matplotlib.widgets import Slider, RadioButtons, TextBox
 from mpl_toolkits.mplot3d import Axes3D
 
 from src.dataset import SyntheticDataset, DechorateDataset
@@ -23,9 +23,10 @@ imag_dset.set_c(343)
 imag_dset.set_k_order(3)
 imag_dset.set_k_reflc(7)
 
+i, j = 0, 0
 
-real_dset.set_dataset('000000')
-real_dset.set_entry(0, 0)
+real_dset.set_dataset('011111')
+real_dset.set_entry(i, j)
 mic_pos, src_pos = real_dset.get_mic_and_src_pos()
 times, h_rec = real_dset.get_rir()
 
@@ -70,12 +71,17 @@ sl_speed = Slider(ax_speed, 'velocity', 335, 350, valinit=343, valstep=0.5)
 sl_sxlim = Slider(ax_sxlim, 'x ax start', -0.05, 0.20, valinit=-.001, valstep=0.005)
 sl_fxlim = Slider(ax_fxlim, 'x ax end', -0.05, 0.20, valinit=0.03, valstep=0.005)
 
+rax = plt.axes([0.9, 0.90, 0.03, 0.02])  # x, y, w, h
+txt_box_mic = TextBox(rax, 'mic', '0')
+rax = plt.axes([0.9, 0.945, 0.03, 0.02])  # x, y, w, h
+txt_box_src = TextBox(rax, 'src', '0')
+
+real_dset.set_entry(i, j)
 
 def update(val=None):
     ax1.clear()
     ax2.clear()
 
-    real_dset.set_entry(0, 1)
     mic_pos, src_pos = real_dset.get_mic_and_src_pos()
     times, h_real = real_dset.get_rir()
     h_real = np.abs(h_real)/np.max(np.abs(h_real))
@@ -87,14 +93,21 @@ def update(val=None):
     imag_dset.set_mic(sl_mic_x.val, sl_mic_y.val, sl_mic_z.val)
     imag_dset.set_src(sl_src_x.val, sl_src_y.val, sl_src_z.val)
 
-    amp, tau, _, _ = imag_dset.get_note()
+    amp, tau, wall, order = imag_dset.get_note()
+    K = len(amp)
+
     times, h_imag = imag_dset.get_rir()
+
+    for k in range(K):
+        ax1.annotate(r'$\tau_{%s}^{%d}$' % (wall[k], order[k]), [tau[k], amp[k]], fontsize=12)
 
     imag_dset.plot_room(ax2)
     ax1.plot(times, h_imag, color='C1')
     ax1.stem(tau, amp, use_line_collection=True)
     ax1.set_xlim([sl_sxlim.val, sl_fxlim.val])
     ax1.set_ylim([-0.01, 1.01])
+
+    ax2.set_title('mic %d / src %d' % (i, j))
 
     fig.canvas.draw_idle()
 
@@ -110,5 +123,7 @@ sl_speed.on_changed(update)
 sl_sxlim.on_changed(update)
 sl_fxlim.on_changed(update)
 bt_dsets.on_clicked(lambda  val: update(real_dset.set_dataset(val)))
+txt_box_mic.on_submit(lambda val: update(real_dset.set_entry(int(val), real_dset.src_j)))
+txt_box_src.on_submit(lambda val: update(real_dset.set_entry(real_dset.src_j, int(val))))
 
 plt.show()
--- src/visualization/interactive_rir2toa.py
+++ src/visualization/interactive_rir2toa.py
@@ -4,8 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import pyroomacoustics as pra
+import peakutils as pk
 
 from copy import deepcopy as cp
+from datetime import datetime
 
 from matplotlib.widgets import Slider, RadioButtons, Button, TextBox, CheckButtons
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,6 +16,7 @@ from src.dataset import SyntheticDataset, DechorateDataset
 from src.utils.file_utils import load_from_pickle, save_to_pickle
 from src.utils.dsp_utils import normalize
 
+from tkinter.filedialog import askopenfilename
 
 dataset_dir = './data/dECHORATE/'
 path_to_processed = './data/processed/'
@@ -26,12 +29,16 @@ datasets = ['000000', '010000', '011000', '011100', '011110', '011111',
             '001000', '000100', '000010', '000001']
 
 
+#TODO: Peak peaking
+#TODO: Save to csv
+
 ## INITIALIZE FIGURE
 scaling = 0.8
 fig = plt.figure(figsize=(16*scaling, 9*scaling))
 ax1 = fig.add_subplot(221, xlim=(20, 2000), ylim=(-0.05, 1.05))
 ax2 = fig.add_subplot(222, xlim=(20, 2000), ylim=(-0.05, 1.05), sharex=ax1, sharey=ax1)
-ax3 = fig.add_subplot(212, xlim=(20, 2000), ylim=(-0.05, 1.05), sharex=ax1, sharey=ax1)
+ax3 = fig.add_subplot(223, xlim=(20, 2000), ylim=(-0.05, 1.05), sharex=ax1, sharey=ax1)
+ax4 = fig.add_subplot(224, sharex=ax1)
 
 plt.subplots_adjust(top=0.90)
 plt.subplots_adjust(left=0.15)
@@ -60,7 +67,7 @@ def plot_rirs_and_note(rirs, note, i, j, selected_k, ax, visibility, dp_extreme)
             rir_to_plot = rirs[:, d]
             rir_to_plot = rir_to_plot / np.max(np.abs(rir_to_plot), axis=0)
 
-            ax.plot(rir_to_plot**2, alpha=.8, label=datasets[d])
+            ax.plot(np.abs(rir_to_plot), alpha=.8, label=datasets[d])
 
     # plot the echo information
     for k in range(K):
@@ -69,17 +76,44 @@ def plot_rirs_and_note(rirs, note, i, j, selected_k, ax, visibility, dp_extreme)
         wall = note['wall'][k, i, j, 0]
         order = note['order'][k, i, j, 0]
         if k == selected_k:
-            ax.axvline(x=int(toa*Fs), c='C5', alpha=1)
+            ax.axvline(x=toa*Fs, c='C5', alpha=1)
         else:
-            ax.axvline(x=int(toa*Fs), c='C0', alpha=0.5)
+            ax.axvline(x=toa*Fs, c='C0', alpha=0.5)
 
-        ax.annotate( r'$\tau_{%s}^{%d}$' % (wall.decode(), order), [toa*Fs, 0.25], fontsize=12)
+        ax.annotate( r'$\tau_{%s}^{%d}$' % (wall.decode(), order), [toa*Fs, 0.05], fontsize=12)
 
     ax.set_title('RIRs dataset %s\nmic %d, src %d' % (datasets[0], i, j))
     pass
 
 
-def plot_rirs_and_note_merged(rirs, note, i, j, selected_k, ax, fun= lambda x : np.mean(x**2, axis=-1)):
+def plot_staked_rirs(rir, note, i, j, selected_k, ax):
+    idx0 = (i // 5)*5
+    idx1 = idx0 + 5
+    idx = i % 5
+
+    stacked_rirs = rir[:, idx0:idx1, j, 5]
+    stacked_rirs = stacked_rirs / np.max(np.abs(stacked_rirs), axis=0)[None, :]
+
+
+    ax.imshow(np.abs(stacked_rirs).T, aspect='auto')
+    for k in range(K):
+        for ii in range(5):
+            toa = note['toa'][k, idx0+ii, j, 0]
+            # amp = note['amp'][k, i, j, 0]
+            # wall = note['wall'][k, i, j, 0]
+            # order = note['order'][k, i, j, 0]
+            if ii == idx:
+                ax.scatter(toa*Fs, ii, c='C3', marker='d', alpha=0.6)
+            else:
+                ax.scatter(toa*Fs, ii, c='C1', marker='o', alpha=0.6)
+
+        # ax.annotate(r'$\tau_{%s}^{%d}$' %
+        #             (wall.decode(), order), [toa*Fs, 0.05], fontsize=12)
+    pass
+
+
+
+def plot_rirs_and_note_merged(rirs, note, i, j, selected_k, ax, fun= lambda x : np.mean(np.abs(x), axis=-1)):
 
     mean_rirs_to_plot = fun(rirs)
     mean_rirs_to_plot = normalize(mean_rirs_to_plot)
@@ -91,14 +125,14 @@ def plot_rirs_and_note_merged(rirs, note, i, j, selected_k, ax, fun= lambda x : 
         wall = note['wall'][k, i, j, 0]
         order = note['order'][k, i, j, 0]
         if k == selected_k:
-            ax.axvline(x=int(toa*Fs), c='C5', alpha=1)
+            ax.axvline(x=toa*Fs, c='C5', alpha=1)
         else:
-            ax.axvline(x=int(toa*Fs), c='C0', alpha=0.5)
+            ax.axvline(x=toa*Fs, c='C0', alpha=0.5)
 
         ax.annotate(r'$\tau_{%s}^{%d}$' % (wall.decode(), order), [
-                    toa*Fs, 0.25], fontsize=12)
+                    toa*Fs, 0.05], fontsize=12)
 
-    pass
+    return mean_rirs_to_plot
 
 
 ## EDIT PLOT VISIBILITY
@@ -123,8 +157,13 @@ rax = plt.axes([0.93, 0.88, 0.05, 0.03])
 dp_box = Button(rax, 'Do it')
 
 # Buttons
-# ax_save_tau = plt.axes([0.01, 0.90, 0.04, 0.02])  # x, y, w, h
-# bt_save_tau = Button(ax_save_tau, 'save')
+ax_save_tau = plt.axes([0.88, 0.05, 0.10, 0.02])  # x, y, w, h
+bt_save_tau = Button(ax_save_tau, 'save tau to csv')
+ax_load_tau = plt.axes([0.88, 0.08, 0.10, 0.02])  # x, y, w, h
+bt_load_tau = Button(ax_load_tau, 'load tau csv')
+
+ax_peak_tau = plt.axes([0.88, 0.45, 0.10, 0.02])  # x, y, w, h
+bt_peak_tau = Button(ax_peak_tau, 'Run peak')
 
 # Text
 ax_text_mic = plt.axes([0.06, 0.95, 0.05, 0.03])  # x, y, w, h
@@ -132,6 +171,12 @@ tx_text_mic = TextBox(ax_text_mic, 'Set mic ', initial='0')
 ax_text_src = plt.axes([0.06, 0.915, 0.05, 0.03])  # x, y, w, h
 tx_text_src = TextBox(ax_text_src, 'Set src ', initial='0')
 
+rax = plt.axes([0.93, 0.200, 0.05, 0.03])  # x, y, w, h
+txt_boxes_pck_dst = TextBox(rax, 'distance ', initial='10')
+rax = plt.axes([0.93, 0.235, 0.05, 0.03])  # x, y, w, h
+txt_boxes_pck_thr = TextBox(rax, 'threshold', initial='0.03')
+rax = plt.axes([0.93, 0.270, 0.05, 0.03])  # x, y, w, h
+txt_boxes_pck_num = TextBox(rax, 'num_peaks', initial='10')
 
 class Callbacks():
 
@@ -148,6 +193,14 @@ class Callbacks():
         self.Fs = 48000
         self.k = 0
 
+        self.peak = {
+            'distance' : 10,
+            'threshold' : 0.03,
+            'num_peaks' : 10,
+        }
+
+        self.feature = None
+
         self.dp_extreme = [int(self.toa_note['toa'][0, self.i, self.j, 0]*self.Fs)-10,
                            int(self.toa_note['toa'][0, self.i, self.j, 0]*self.Fs)-30]
 
@@ -162,18 +215,22 @@ class Callbacks():
         self._update()
 
 
-    def save_tau_to_file(self, foo):
+    def save_tau_to_file(self, event):
         path = './data/interim/manual_annotation/'
-        filename = 'src-%d-mic-%d.pkl' % (self.j, self.i)
-        save_to_pickle(path + filename, self.toa_note)
-        print('Annotation saved to file:', filename)
+        filename = "%s_gui_annotation" % datetime.now().strftime('%Y%m%d_%Hh%M')
+        save_to_pickle(path + filename + '.pkl', self.toa_note)
+        foo = self.toa_note['toa'][:7,:,:,0].reshape([7,I*J])
+        np.savetxt(path+filename+'.csv', foo, delimiter=',')
+        print('Annotation saved to file:', path+filename)
+        pass
 
 
     def load_tau_note(self, bar):
-        path = './data/interim/manual_annotation/'
-        filename = 'src-%d-mic-%d.pkl' % (self.j, self.i)
-        self.toa_note = load_from_pickle(path + filename)
+        filename = askopenfilename()
+        print(self.toa_note['toa'][:, self.i, self.j, 0])
+        self.toa_note = load_from_pickle(filename)
         print('Annotation loaded from:', filename)
+        print(self.toa_note['toa'][:, self.i, self.j, 0])
         self._update()
 
 
@@ -208,6 +265,7 @@ class Callbacks():
     def set_extreme_dp_deconvolution(self, x, i):
         self.dp_extreme[i] = x
         print('Set %d extreme to %d' % (i, x))
+        self._update()
         self.axes[0].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
         self.axes[1].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
         self.axes[2].fill_between(np.arange(self.dp_extreme[0], self.dp_extreme[1]), -0.2, 1.2, color='C8', alpha=0.2)
@@ -241,10 +299,39 @@ class Callbacks():
         # update taus
         self.idx = i
         self.toa_note['toa'][self.idx, self.i, self.j, :] = val/self.Fs
-        print('Updated tau %d to %1.2f' %
-              (self.idx, self.toa_note['toa'][self.idx, self.i, self.j, 0]))
+        tmp = self.toa_note['toa'][self.idx, self.i, self.j, 0]
+        print('Updated tau %d to %1.2f [s] %1.6f' %
+              (self.idx, tmp, tmp*self.Fs))
         self._update()
 
+
+    def set_peak_dict(self, key, val):
+        self.peak[key] = val
+        print('Registered change', key, self.peak[key])
+
+
+    def peak_peaking(self, event):
+        # run peak piking of the clean rirs
+        print('Run peak picking')
+        print(self.feature)
+        if self.feature is None:
+            pass
+        cb = cp(self.feature).squeeze()
+        peaks = pk.indexes(cb, thres=self.peak['threshold'], min_dist=self.peak['distance'])
+        interpolated_peaks = pk.interpolate(np.arange(0, len(cb)), cb, ind=peaks, width=3)
+
+        if len(peaks) == 0:
+            print('No peaks')
+            pass
+
+        peaks = peaks[:self.peak['num_peaks']]
+        self.axes[2].scatter(peaks, cb[peaks])
+
+        print('Peaks found:')
+        for i, p in enumerate(peaks):
+            print('-', i,  p, '', interpolated_peaks[i])
+            self.axes[2].annotate("%d - %1.4f" %(i, interpolated_peaks[i]), [interpolated_peaks[i], cb[p]])
+        self.fig.canvas.draw_idle()
 
     def dp_deconv(self, event):
         L, I, J, D = self.all_rirs.shape
@@ -283,28 +370,25 @@ class Callbacks():
         self.axes[0].clear()
         self.axes[1].clear()
         self.axes[2].clear()
+        self.axes[3].clear()
 
         self.axes[0].set_xlim(xlim)
         self.axes[0].set_ylim(ylim)
 
-        print(self.idx)
         plot_rirs_and_note(self.curr_rirs, self.toa_note, self.i, self.j, self.idx, self.axes[0], self.visibility, self.dp_extreme)
         plot_rirs_and_note(self.curr_rirs_clean, self.toa_note, self.i, self.j, self.idx, self.axes[1], self.visibility, self.dp_extreme)
 
-        # # peak picking of 2nd axis
-        # mean_rirs_to_plot = np.max(self.curr_rirs_clean**2, axis=-1)
-        # mean_rirs_to_plot = normalize(mean_rirs_to_plot)
-        # peaks, _ = sp.signal.find_peaks(mean_rirs_to_plot, prominence=0.05, distance=10)
-        # print(peaks)
-        # self.ax2.scatter(peaks, mean_rirs_to_plot[peaks], color='red', marker='x')
+        self.feature = plot_rirs_and_note_merged(
+            self.curr_rirs_clean, self.toa_note, self.i, self.j, self.idx, self.axes[2])
 
-        plot_rirs_and_note_merged(self.curr_rirs_clean, self.toa_note, self.i, self.j, self.idx, self.axes[2])
+        plot_staked_rirs(self.all_rirs, self.toa_note, self.i, self.j, self.idx, self.axes[3])
 
         self.fig.canvas.draw_idle()
 
 
 # init
-cb = Callbacks(cp(toa_note), all_rirs, cp(all_rirs), datasets, initial_validity, fig, axes=[ax1, ax2, ax3])
+axes = [ax1, ax2, ax3, ax4]
+cb = Callbacks(cp(toa_note), all_rirs, cp(all_rirs), datasets, initial_validity, fig, axes=axes)
 
 check.on_clicked(cb.set_visibility)
 
@@ -312,8 +396,9 @@ dp_box1.on_submit(lambda string: cb.set_extreme_dp_deconvolution(int(string), 0)
 dp_box2.on_submit(lambda string: cb.set_extreme_dp_deconvolution(int(string), 1))
 dp_box.on_clicked(cb.dp_deconv)
 
-# bt_save_tau.on_clicked(cb.save_tau_to_file)
-# bt_load_tau.on_clicked(cb.load_tau_note)
+bt_save_tau.on_clicked(cb.save_tau_to_file)
+bt_load_tau.on_clicked(cb.load_tau_note)
+bt_peak_tau.on_clicked(cb.peak_peaking)
 
 tx_text_mic.on_submit(lambda i: cb.set_mic(int(i)))
 tx_text_src.on_submit(lambda j: cb.set_src(int(j)))
@@ -325,5 +410,10 @@ txt_boxes_tau[3].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 3))
 txt_boxes_tau[4].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 4))
 txt_boxes_tau[5].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 5))
 txt_boxes_tau[6].on_submit(lambda tau_str: cb.set_tau_val(float(tau_str), 6))
+
+
+txt_boxes_pck_dst.on_submit(lambda val_str: cb.set_peak_dict('distance', float(val_str)))
+txt_boxes_pck_thr.on_submit(lambda val_str: cb.set_peak_dict('threshold', float(val_str)))
+txt_boxes_pck_num.on_submit(lambda val_str: cb.set_peak_dict('num_peaks', float(val_str)))
 
 plt.show()
