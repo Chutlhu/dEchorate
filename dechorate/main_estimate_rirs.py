879c5289cf190f71edcaffb90b880f7770865b6c
Chutlhu <diego.dicarlo89@gmail.com>
2022-04-15 11:24:09 +0200

    ready for cluster

---
+++ dechorate/main_estimate_rirs.py
@@ -0,0 +1,165 @@
+import h5py
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+from pyrsistent import b
+from tqdm import tqdm
+
+from dechorate import constants # these are stored in the __init__py file
+from dechorate.stimulus import ProbeSignal
+from dechorate.utils.dsp_utils import *
+
+rec_offset = constants['recording_offset']
+L = int(5*constants['Fs'])
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dbpath", help="Path to dEchorate database", type=str)
+    parser.add_argument("--chirps", help="Path to dEchorate_chirp.hdf5", type=str)
+    args = parser.parse_args()
+
+    curr_dset_name = 'dEchorate_rirs'
+
+    # setup paths
+    path_to_output = Path('.', 'outputs')
+    path_to_annotation = Path(args.dbpath)
+    path_to_chirps = Path(args.chirps)
+    
+    # get constants and values
+    room_codes = constants['datasets']
+    Fs = constants['Fs']
+    src_ids = constants['src_ids']
+    mic_ids = constants['mic_ids']
+
+    # open the database
+    df_note = pd.read_csv(path_to_annotation)
+
+    # open the dataset of chirsp
+    dset_chirp = h5py.File(path_to_chirps, "r")
+
+    ## INITIALIZE THE HDF5 DATASET
+    # signal = ['chirp', 'silence', 'babble', 'speech', 'noise']
+
+    # DATASET structure: room x mics x srcs + bonus
+    # room = [000000, 010000, ..., 111111, 020002]
+    # mics = [0, ..., 29, 30] : 0-29 capsule, 30th loopback
+    # srcs = [0, ..., 8] : 6 dir,  3 omni
+    # bonus: book for polarity
+
+    path_to_output_dataset_hdf5 = path_to_output / Path(f'{curr_dset_name}.hdf5')
+    if not path_to_output_dataset_hdf5.exists():
+        f = h5py.File(path_to_output_dataset_hdf5, 'w')
+        f.close()
+    # re-open it in append mode
+    hdf = h5py.File(path_to_output_dataset_hdf5, 'a')
+
+    ## POPULATE THE HDF5 DATASET
+
+    # navigate the chirps dataset
+    dt = {
+        'signal' : []
+    ,   'room_code' : []
+    ,   'src_id' : []
+    ,   'mic_id' : []
+    ,   'path_hdf5' : []
+    ,   'delay' : []                 
+    }
+
+    for room_code in tqdm(room_codes, desc="room_code"):
+
+        for src_id in tqdm(src_ids, desc="src_id"):
+
+            group = f'/chirp/{room_code}/{src_id:d}'
+            data = np.array(dset_chirp[group])
+        
+            loopback = data[:,-1]
+
+            # RIR estimation
+            Fs = constants['rir_processing']['Fs']
+            assert Fs == constants['Fs']
+            n_seconds = constants['rir_processing']['n_seconds']
+            amplitude = constants['rir_processing']['amplitude']
+            n_repetitions = constants['rir_processing']['n_repetitions']
+            silence_at_start = constants['rir_processing']['silence_at_start']
+            silence_at_end = constants['rir_processing']['silence_at_end']
+            sweeprange = constants['rir_processing']['sweeprange']
+            stimulus = constants['rir_processing']['stimulus']
+            ps = ProbeSignal(stimulus, Fs)
+            times, s = ps.generate(n_seconds, amplitude, n_repetitions, silence_at_start, silence_at_end, sweeprange)
+            
+            # compute the global delay from the rir with the playback signal
+            try:
+                rir_loop = ps.compute_rir(loopback[:,None], windowing=False)
+                delay = np.argmax(np.abs(rir_loop)) # in samples
+            except:
+                # print(delay)
+                delay = 999999
+
+            n_mics = data.shape[1]
+            rirs = np.zeros([5*Fs, n_mics])
+
+            group = f'/rir/{room_code}/{src_id:d}'
+
+            for i in tqdm(range(n_mics), desc="i"):
+
+                if int(room_code) == 20002:
+                    curr_room_code = 20002
+                    curr_fornitures = True
+                else:
+                    curr_room_code = room_code
+                    curr_fornitures = False
+
+                info = df_note.loc[
+                    (df_note['src_id'] == float(src_id))
+                &   (df_note['src_signal'] == 'chirp')
+                &   (df_note['room_code'] == int(curr_room_code))
+                &   (df_note['room_fornitures'] == curr_fornitures)
+                &   (df_note['mic_id'] == float(i+1))
+                ]
+
+                try:
+                    assert len(info) == 1
+                except:
+                    print(room_code, curr_room_code, src_id, i+1)
+                    print(info)
+                    hdf.close()
+
+                # # compute the rir
+                if group in hdf:
+                    continue
+                else:
+                    rir = ps.compute_rir(data[:, i, None], windowing=False)
+                    rirs[:,i] = rir[0:int(5*Fs)].squeeze()
+
+
+                dt['signal'].append('rir')
+                dt['room_code'].append(room_code)
+                dt['src_id'].append(src_id)
+                dt['mic_id'].append(i)
+                dt['path_hdf5'].append(group)
+                dt['delay'].append(delay)
+
+            # compensate delays (hardcoded in __init__.py)
+            if group in rec_offset.keys():
+                d = rec_offset[group]
+            else:
+                d = rec_offset['standard']
+
+            rirs = rirs[d:d+L,:]
+            
+            if group in hdf:
+                continue
+            else:
+                hdf.create_dataset(group, data=rirs)
+
+            df = pd.DataFrame(dt)
+            df.to_csv(path_to_output / Path('dEchorate_rir_database.csv'))
+            
+
+    hdf.close()
---
+++ run_deploy_dataset_from_raws.sh
@@ -0,0 +1,32 @@
+# Paths
+path_to_data_dir="/home/chutlhu/Documents/Datasets/dEchorate/raw/"
+
+
+# # 1. annotation files to database
+python dechorate/main_geometry_from_measurements.py $path_to_data_dir
+
+# # 2. Final calibrated geometry
+python dechorate/main_geometry_from_echo_calibration.py $path_to_data_dir
+
+# # 3. Build the complete database with recordings 
+path_to_calibrated_positions_notes='outputs/dEchorate_calibrated_elements_positions.csv'
+python dechorate/main_build_annotation_database.py $path_to_data_dir $path_to_calibrated_positions_notes
+
+# # 4. Build sound dataset: from zips to hdf5
+path_to_database="outputs/dEchorate_database.csv"
+python dechorate/main_build_sound_datasets.py --signal silence --datadir $path_to_data_dir --dbpath $path_to_database
+python dechorate/main_build_sound_datasets.py --signal babble  --datadir $path_to_data_dir --dbpath $path_to_database
+python dechorate/main_build_sound_datasets.py --signal noise   --datadir $path_to_data_dir --dbpath $path_to_database
+python dechorate/main_build_sound_datasets.py --signal speech  --datadir $path_to_data_dir --dbpath $path_to_database
+python dechorate/main_build_sound_datasets.py --signal chirp   --datadir $path_to_data_dir --dbpath $path_to_database
+echo "you may want to deleat the content of .cache folder" 
+
+# # 5. Estimate RIRs
+path_to_chirps="outputs/dEchorate_chirp.hdf5"
+python dechorate/main_estimate_rirs.py --dbpath $path_to_database --chirps $path_to_chirps
+
+
+# # 6. Repack datasets
+for signal in silence babble noise speech chirp rirs; do
+    h5repack -v -f GZIP=7 outputs/dEchorate_${signal}.hdf5 outputs/dEchorate_${signal}_gzip7.hdf5
+done
